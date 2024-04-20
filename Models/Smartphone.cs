7b85dd13a093a8ff3f9fbe17ab020e60ee977bd1
Jhonny Ferreira <jhonnysossai@gmail.com>
2024-04-19 22:30:38 -0300

    Finalização do Desafio da trilha de POO

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,17 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
+        protected List<string> ListaAplicativos = [];
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;    
         }
 
         public void Ligar()
@@ -21,6 +26,32 @@ namespace DesafioPOO.Models
             Console.WriteLine("Recebendo ligação...");
         }
 
+        public void AtenderLigacao()
+        {
+            Console.WriteLine("Ligação Atendida");
+        }
+
+        public void RecusarLigacao()
+        {
+            Console.WriteLine("Ligação recusada");
+        }
+
         public abstract void InstalarAplicativo(string nomeApp);
+        public void ExibirAplicativos()
+        {
+            if(ListaAplicativos.Count() == 0)
+            {
+                Console.WriteLine("Ainda não foram instalador aplicativos");
+            }
+            else
+            {
+                Console.WriteLine("- Aplicativos intalados -");
+                foreach(var lista in ListaAplicativos)
+                {
+                    Console.WriteLine(" - " + lista);
+                }
+            }
+            
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("98763-0912", "C20", "001970-01-010000-0", 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.RecusarLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+nokia.ExibirAplicativos();
+
+Console.WriteLine("\n");
+
+Iphone iphone = new Iphone("8888-8888", "14", "098292-02-120001-0", 32);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+iphone.InstalarAplicativo("Waze");
+iphone.ExibirAplicativos();
